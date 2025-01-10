e42954137369d7db9176aec8c6c241cf62acee07
Xiaotong Liu <liuxiaotong76@outlook.com>
2025-01-10 15:58:08 +0100

    simulating different conditions

--- simulating_different_conditions.R
+++ simulating_different_conditions.R
@@ -5,42 +5,42 @@ source("simulation_function.R")
 # condition 1: a high, b low, condition b|a high
 
 # condition 2: a high, b high, condition b|a high
-run_simulation(nsim = 10, 
+run_simulation(nsim = 1e4, 
                alpha_a = 20, beta_a = 5,
                alpha_b = 20, beta_b = 5, 
                alpha_ba = 20, beta_ba = 5,
                condition_name = "condition2", seed = 1234)
 
 # condition 3: a high b low, condition b|a low
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 20, beta_a = 5, 
                alpha_b = 5, beta_b = 20, 
                alpha_ba = 5, beta_ba = 20, 
                condition_name = "condition3", seed = 1234)
 
 # condition 4: a high b high, condition b|a low
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 20, beta_a = 5, 
                alpha_b = 20, beta_b = 5, 
                alpha_ba = 5, beta_ba = 20, 
                condition_name = "condition4", seed = 1234)
 
 # condition 5: a low b low, condition b|a high
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 5, beta_a = 20, 
                alpha_b = 5, beta_b = 20, 
                alpha_ba = 20, beta_ba = 5, 
                condition_name = "condition5", seed = 1234)
 
 # condition 6: a low, b high, condition b|a high
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 5, beta_a = 20, 
                alpha_b = 20, beta_b = 5, 
                alpha_ba = 20, beta_ba = 5, 
                condition_name = "condition6", seed = 1234)
 
 # condition 7: a low b low, condition b|a low
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 5, beta_a = 20, 
                alpha_b = 5, beta_b = 20, 
                alpha_ba = 5, beta_ba = 20, 
@@ -48,7 +48,7 @@ run_simulation(nsim = 10,
 
 
 # condition 8: a low b high, condition b|a low
-run_simulation(nsim = 10,
+run_simulation(nsim = 1e4,
                alpha_a = 5, beta_a = 20, 
                alpha_b = 20, beta_b = 5, 
                alpha_ba = 5, beta_ba = 20, 
