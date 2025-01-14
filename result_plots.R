8f3ff6a97e2b78338d13e6be8ccffc656bcb6c24
Xiaotong Liu <liuxiaotong76@outlook.com>
2025-01-14 15:50:48 +0100

    scripts for simulation result plots

--- result_plots.R
+++ result_plots.R
@@ -23,9 +23,44 @@ df_condition8 <- read.csv("condition8_results.csv") #condition 8: a low b high, 
 #If an event B causes A, then by definition P(A|B) > P(A) (the probability of A, given the causing event B is higher than the probability of A simpliciter)
 # --> this prediction cannot be tested with the current data 
 
-#Prediction 3: the rate at which the conjunctiona falalcy occurs 
+#Prediction 3: the rate at which the conjunctions fallacy occurs 
 #average *estimated* value for the conjunction P(A and B) never be greater than the average *estimated* value for its constituent *P(A) 
 # --> as a consequence, an individual estimate of P(A and B) can randomly fall above an estiamte of P(A) at most 50% of the time 
 # <-- data:: there are some pairs of events for which fallacy rates are signiﬁcantly higher than this bound (Tversky and Kahneman’s Linda and Bill being two examples).
 
-#Let us start with prediction 3: 
+#Let us start with prediction 3 to check the rate at which the conjunction fallacy occurs
+# Vector of conditions
+conditions <- paste0("df_condition", 1:8)
+
+# Loop over each condition and create the summary DataFrame
+for (cond in conditions) {
+  # Get the current DataFrame
+  df <- get(cond)
+  
+  # Create the summary DataFrame
+  summary_df <- df %>% 
+    mutate(
+      mean_logical_with_ties = mean_R1_with_ties + mean_R2_with_ties,
+      mean_singleconjunction_with_ties = mean_R3_with_ties + mean_R4_with_ties,
+      mean_doubleconjucntion_with_ties = mean_R5_with_ties + mean_R6_with_ties,
+      mean_conjunctions = mean_singleconjunction_with_ties + mean_doubleconjucntion_with_ties
+    ) %>% 
+    select(sample_size_in_simulaton,
+           mean_logical_with_ties,
+           mean_singleconjunction_with_ties,
+           mean_doubleconjucntion_with_ties,
+           mean_conjunctions)
+  
+  # Assign the summary DataFrame with a new name
+  assign(paste0(cond, "_summary"), summary_df)
+}
+
+view(df_condition1_summary)
+view(df_condition2_summary)
+view(df_condition3_summary)
+view(df_condition4_summary)
+view(df_condition5_summary)
+view(df_condition6_summary)
+view(df_condition7_summary)
+view(df_condition8_summary)
+
