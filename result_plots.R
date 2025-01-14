44b60b677763a6d0ca10ad3dee67387adf596642
Xiaotong Liu <liuxiaotong76@outlook.com>
2025-01-14 15:05:26 +0100

    prepare the simulation result plots

--- result_plots.R
+++ result_plots.R
@@ -1,15 +1,31 @@
 library(ggplot2)
 library(tidyverse)
 
-df_condition1 <- read.csv("condition1_results.csv")
-df_condition2 <- read.csv("condition2_results.csv")
-df_condition3 <- read.csv("condition3_results.csv")
-df_condition4 <- read.csv("condition4_results.csv")
-df_condition5 <- read.csv("condition5_results.csv")
-df_condition6 <- read.csv("condition6_results.csv")
-df_condition7 <- read.csv("condition7_results.csv")
-df_condition8 <- read.csv("condition8_results.csv")
+df_condition1 <- read.csv("condition1_results.csv") #condition 1: a high, b low, condition b|a high
+df_condition2 <- read.csv("condition2_results.csv") #condition 2: a high, b high, condition b|a high
+df_condition3 <- read.csv("condition3_results.csv") #condition 3: a high b low, condition b|a low
+df_condition4 <- read.csv("condition4_results.csv") #condition 4: a high b high, condition b|a low
+df_condition5 <- read.csv("condition5_results.csv") #condition 5: a low b low, condition b|a high
+df_condition6 <- read.csv("condition6_results.csv") #condition 6: a low, b high, condition b|a high
+df_condition7 <- read.csv("condition7_results.csv") #condition 7: a low b low, condition b|a low
+df_condition8 <- read.csv("condition8_results.csv") #condition 8: a low b high, condition b|a low
 
+#Question: if the rates at which the conjunction fallacy occurs different across conditions? 
 
+#From Costello & Watt's papers: 
+#Prediction 1: the influence of the distance between underlying probabilities 
+#original PT+N model predicts that the closest P(A and B) to P(A) the, the greater the chance the conjunction fallacy would occur 
+# <-- these values are closet when P(A) is low and both P(B) and P(A|B) are high 
+# <-- supporting data: (Carlson & Yates, 1989; Costello, 2009b; Fantino et al., 1997; Gavanski & Roskos-Ewoldsen, 1991)
+# --> this prediction can be tested with the current simualtion data 
 
+#Prediction 2: the influence of the distance between underlying probabilities 
+#If an event B causes A, then by definition P(A|B) > P(A) (the probability of A, given the causing event B is higher than the probability of A simpliciter)
+# --> this prediction cannot be tested with the current data 
 
+#Prediction 3: the rate at which the conjunctiona falalcy occurs 
+#average *estimated* value for the conjunction P(A and B) never be greater than the average *estimated* value for its constituent *P(A) 
+# --> as a consequence, an individual estimate of P(A and B) can randomly fall above an estiamte of P(A) at most 50% of the time 
+# <-- data:: there are some pairs of events for which fallacy rates are signiﬁcantly higher than this bound (Tversky and Kahneman’s Linda and Bill being two examples).
+
+#Let us start with prediction 3: 
